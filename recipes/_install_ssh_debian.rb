42d4d4613166a62492afeda7e899d0b6cfc56e75
Matthew Cincotti <mcincotti@conjur.net>
2016-01-13 15:45:03 -0500

    Cleaning code up.  The difference between Debian and Ubuntu requirements is 1 package.

--- recipes/_install_ssh_debian.rb
+++ recipes/_install_ssh_debian.rb
@@ -11,22 +11,15 @@ end
 
 execute "debconf-set-selections /tmp/ldap.seed"
 
-case node['platform']
-  when 'ubuntu'
-    for pkg in %w(debconf nss-updatedb nscd libpam-mkhomedir auth-client-config ldap-utils ldap-client libpam-ldapd libnss-ldapd)
-      package pkg do
-        options "-qq"
-      end
-    end
-  when 'debian'
-    for pkg in %w(debconf nss-updatedb nscd libpam-mkhomedir ldap-utils ldap-client libpam-ldapd libnss-ldapd )
-      package pkg do
-        options "-qq"
-      end
-    end
-  else
-    raise "Unsupported platform : #{node['platform']}"
+pkgs = %w(debconf nss-updatedb nscd libpam-mkhomedir ldap-utils ldap-client libpam-ldapd libnss-ldapd)
+if node['platform'] == 'ubuntu'
+  pkgs << 'auth-client-config'
 end
+for pkg in pkgs
+      package pkg do
+        options "-qq"
+      end
+    end
 
 execute "pam-auth-update" do
   command "pam-auth-update --package"
