7e5aa265a665924e0f67cc4608d7bed123b3af87
Alan Potter <alanjpotter@gmail.com>
2016-01-17 10:24:53 -0500

    Install oddjob and oddjob-mkhomedir on RHEL 7

--- recipes/_install_ssh_rhel.rb
+++ recipes/_install_ssh_rhel.rb
@@ -1,4 +1,14 @@
-%w(nscd openldap openldap-clients nss-pam-ldapd authconfig openssl-perl policycoreutils-python).each do |pkg|
+# amazon linux uses 'current' as $releasever
+# but packages from EL7 work on it.
+# CentOS and RHEL actually have '6' or '7' in the variable.
+releasever = {
+  'amazon' => '7'
+}[node.platform] || '$releasever'
+
+package_list=%w(nscd openldap openldap-clients nss-pam-ldapd authconfig openssl-perl policycoreutils-python oddjob)
+package_list << "oddjob-mkhomedir" if Chef::VersionConstraint.new('~> 7.0').include?(node['platform_version'])
+
+package_list.each do |pkg|
   package pkg do
     options '-y'
   end
@@ -13,13 +23,6 @@ cookbook_file '/etc/pki/rpm-gpg/RPM-GPG-KEY-Conjur' do
   mode '644'
   source 'apt.key'
 end
-
-# amazon linux uses 'current' as $releasever
-# but packages from EL7 work on it.
-# CentOS and RHEL actually have '6' or '7' in the variable.
-releasever = {
-  'amazon' => '7'
-}[node.platform] || '$releasever'
 
 yum_repository 'conjur' do
   description 'Conjur Inc.'
