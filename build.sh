a28af96f6fc1223258190f1890ff50b4f906a49e
Alan Potter <alanjpotter@gmail.com>
2016-02-15 07:54:34 -0500

    Name the packaged cookbook tarball conjur.tar.gz for easier publishing. Clean up some unused files

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ docker build -t ci-conjur-cookbook${buildno} .
       
 # Take advantage of the docker layer cache to work around the fact
 # that berks package isn't idempotent.
-docker build -t ci-cookbook-storage -f docker/Dockerfile.cookbook .
-docker run -i --rm -v $PWD/ci/output:/src/output ci-cookbook-storage mv /cookbooks/conjur.tar.gz /src/output/cookbooks.tar.gz
+docker build -t ci-cookbook-storage -f Dockerfile.cookbook .
+docker run -i --rm -v $PWD/ci/output:/src/output ci-cookbook-storage mv /cookbooks/conjur.tar.gz /src/output/conjur.tar.gz
 
 if [ ! -z "$CONJUR_DOCKER_REGISTRY" ]; then
   img=$(./image_name.sh)
