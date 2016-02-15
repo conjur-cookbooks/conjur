785949630f99f53f6c48ea2b9f949754723a356e
Alan Potter <alanjpotter@gmail.com>
2016-02-15 16:00:29 -0500

    switch back to 'conjur audit all -s'

--- test/integration/default/serverspec/configure_spec.rb
+++ test/integration/default/serverspec/configure_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-describe command('conjur audit all') do
-  its(:stdout) { should match(/"action": "login"/) }
+describe command('conjur audit all -s') do
+  its(:stdout) { should match(/ssh:login/) }
   its(:exit_status) { should eq 0 }
 end
