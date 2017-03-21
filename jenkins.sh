f855a38e7b47c3284ce00c5003f0ae0dbed50031
Dustin Collins <dustinrcollins@gmail.com>
2017-03-21 14:03:39 -0500

    wip

--- jenkins.sh
+++ jenkins.sh
@@ -1,17 +1,6 @@
 #!/bin/bash -e
 
-readonly CONJUR_VERSION='4.9.0.1'  # these 2 vars are tied together, for now
-readonly CONJUR_AMI='ami-e9d97eff'
 readonly TEST_CONTAINER_TAG='conjur-cookbook-test'
-
-KEEP=${KEEP:-0}  # set to 1 to keep the Conjur EC2 instance running
-
-function finish() {
-  if [ $KEEP -eq 0 ]; then
-    destroy_conjur_instance
-  fi
-}
-trap finish EXIT
 
 function main() {
   build_test_image
@@ -27,9 +16,7 @@ function build_test_image() {
 
 function testC() {
   # Run a command inside the test container
-  docker run --rm -i \
-    -v $PWD:/src -w /src \
-    $TEST_CONTAINER_TAG "$@"
+  docker run --rm -i -v $PWD:/src -w /src $TEST_CONTAINER_TAG "$@"
 }
 
 function check_syntax() {
@@ -52,14 +39,6 @@ function lint_cookbook() {
 function run_specs() {
   echo 'Running rspec unit tests'
   testC rspec --format documentation --format RspecJunitFormatter --out ci/reports/specs.xml spec/
-}
-
-function create_conjur_instance() {
-  echo 'Starting Conjur EC2 instance'
-}
-
-function destroy_conjur_instance() {
-  echo 'Destroying Conjur EC2 instance'
 }
 
 main
---
+++ jenkins_acceptance.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+SUITE=${1:-all}  # which test-kitchen suite to run, all by default
+KEEP=${KEEP:-0}  # set to 1 to keep the Conjur EC2 instance running
+
+readonly CONJUR_AMI='ami-e9d97eff'  # Conjur 4.9.0.1
+readonly TEST_CONTAINER_TAG='conjur-cookbook-acceptance-test'
+readonly CONCURRENCY=10  # number of test-kitchen suites to run in parallel
+
+function finish() {
+  if [ $KEEP -eq 0 ]; then
+    testC kitchen destroy -c $CONCURRENCY $SUITE
+  fi
+}
+trap finish EXIT
+
+function main() {
+  build_test_image
+  run_kitchen_tests
+}
+
+function build_test_image() {
+  echo 'Building test Docker image'
+  docker build -f Dockerfile.kitchen -t $TEST_CONTAINER_TAG .
+}
+
+function testC() {
+  # Run a command inside the test container
+  summon -f secrets.ci.yml \
+    docker run --rm \
+      --env-file @SUMMONENVFILE \
+      -e "AWS_PRIVATE_KEY=$(conjur variable value aws/ci/test-kitchen/private-key)" \
+      -v $PWD:/src -w /src \
+      $TEST_CONTAINER_TAG "$@"
+}
+
+
+function run_kitchen_tests() {
+  testC kitchen create -c $CONCURRENCY $SUITE
+  testC kitchen test -c $CONCURRENCY $SUITE
+}
+
+main
