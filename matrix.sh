ec8d7e1aac6653eabf57279ca4719a0cd5d248f5
Dustin Collins <dustinrcollins@gmail.com>
2017-03-22 09:48:47 -0500

    Use matrix.sh for backwards compatibility

--- matrix.sh
+++ matrix.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
 
-SUITE=${1:-all}  # which test-kitchen suite to run, all by default
-KEEP=${KEEP:-0}  # set to 1 to keep the Conjur EC2 instance running
+KITCHEN_ARGS="$@"  # arguments to pass directly to test kitchen, e.g., '--only ubuntu-16'
+KEEP=${KEEP:-0}  # env var, set to 1 to keep the Conjur EC2 instance running
 
 readonly CONJUR_AMI='ami-e9d97eff'  # Conjur 4.9.0.1
 readonly TEST_CONTAINER_TAG='conjur-cookbook-acceptance-test'
-readonly CONCURRENCY=10  # number of test-kitchen suites to run in parallel
 
 function finish() {
   if [ $KEEP -eq 0 ]; then
-    testC kitchen destroy -c $CONCURRENCY $SUITE
+    testC kitchen destroy $SUITE
   fi
 }
 trap finish EXIT
@@ -36,7 +35,7 @@ function testC() {
 
 
 function run_kitchen_tests() {
-  testC kitchen test -c $CONCURRENCY $SUITE
+  testC kitchen test $KITCHEN_ARGS
 }
 
 main
