dcfee80b1533c7aafb205208dbaa38cab02a21bf
Rafał Rzepecki <divided.mind@gmail.com>
2018-02-05 17:39:15 -0500

    Workaround for xenial bug preventing mkhomedir from working

--- recipes/_install_ssh_debian.rb
+++ recipes/_install_ssh_debian.rb
@@ -5,10 +5,6 @@ cookbook_file "/tmp/ldap.seed" do
   source "ldap.seed"
 end
 
-cookbook_file "/usr/share/pam-configs/mkhomedir" do
-  source "mkhomedir"
-end
-
 execute "debconf-set-selections /tmp/ldap.seed"
 
 pkgs = %w(debconf nss-updatedb nscd libpam-mkhomedir ldap-utils ldap-client libpam-ldapd libnss-ldapd)
@@ -20,6 +16,24 @@ for pkg in pkgs
         options "-qq"
       end
     end
+
+cookbook_file "/usr/share/pam-configs/mkhomedir" do
+  source "mkhomedir"
+end
+
+# pam-auth-update is broken on xenial, see
+# https://bugs.launchpad.net/ubuntu/+source/pam/+bug/682662
+execute 'workaround Ubuntu bug #682662' do
+  command "sed -i '/mkhomedir/d' /var/lib/pam/seen"
+  only_if do
+    # annoyingly the bug is marked WONTFIX so might as well do it for all ubuntus >= 16.04
+    [
+      (node['platform'] == 'ubuntu'),
+      (node['platform_version'] >= '16.04'),
+      File.exists?('/var/lib/pam/seen'),
+    ].all?
+  end
+end
 
 execute "pam-auth-update" do
   command "pam-auth-update --package"
